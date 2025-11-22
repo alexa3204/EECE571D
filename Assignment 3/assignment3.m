8945c1f29e7d9db44d4150e0e3e5ea766c260b7c
alexapf@student.ubc.ca <alexapf@student.ubc.ca>
2025-11-21 19:07:52 -0800

    assignment 3

--- Assignment 3/assignment3.m
+++ Assignment 3/assignment3.m
@@ -4,4 +4,7 @@ open('Assignment3_2_MMC.slx')
 Rarm = 0; % no arm resistace 
 Larm = 3e-3; % arm inductance
 C_sm = 5e-3; % submodule capacitance 
-V_sm = 1e3; % initial cap voltage per sm 
+V_sm = 2e3; % initial cap voltage per sm 
+
+fc = 1650; % carrier frequency 
+T = 1/fc; 
